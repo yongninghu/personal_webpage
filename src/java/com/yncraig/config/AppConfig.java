95aa76559f0264acc915a4a3949ecce2d74ab035
Craig Hu <craighu@Craigs-MacBook-Pro.local>
2016-11-26 13:01:00 -0800

    initial commit

---
+++ src/java/com/yncraig/config/AppConfig.java
@@ -0,0 +1,16 @@
+package java.com.yncraig.config;
+
+/**
+ * Created by craighu on 11/26/16.
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+public class AppConfig {
+
+}
